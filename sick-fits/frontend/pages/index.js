cdfb815e12af920d91cb8f7438afb88f6cda725c
Rafael Moura <rafael.mouraweb@gmail.com>
2019-04-17 21:56:44 -0300

    criando rotas com nextJS

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import Link from 'next/link'
 
 const Home = props => (
-  <div>Home</div>
+  <div>
+    <p>Sell</p>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+  </div>
 );
 
 
