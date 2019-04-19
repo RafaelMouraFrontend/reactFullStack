f9a1010565229c573e03e925c7cd77c100667885
Rafael Moura <rafael.mouraweb@gmail.com>
2019-04-19 18:03:15 -0300

    inject global and fixing style in server render

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,24 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <div>
+  <NavStyles>
+    <Link href="/itens">
+      <a>Itens</a>
+    </Link>
     <Link href="/sell">
-      <a>Sell!</a>
+      <a>Sell</a>
     </Link>
-    <Link href="/">
-      <a>Home!</a>
+    <Link href="/signup">
+      <a>Signup</a>
     </Link>
-  </div>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav;
